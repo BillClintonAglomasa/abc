b60d22fe0de31f7dc70c85c159a04a7abddfae48
BillClintonAglomasa <aglomasa@yahoo.com>
2024-01-02 16:47:58 +0000

    Create chaos.py that takes two numbers

--- Python_Programming_John_Zelle/Chapter1/chaos.py
+++ Python_Programming_John_Zelle/Chapter1/chaos.py
@@ -3,10 +3,13 @@
 
 def main():
     print("This program illustrates a chaotic function")
-    x = eval(input("Enter a number between 0 and 1: "))
+    x = eval(input("Please enter the first number: "))
+    y = eval(input("Please enter the second number: "))
+    print("input   0.25     0.26")
 
     for i in range(10):
         x = 3.9 * x * (1 - x)
-        print(x)
+        y = 3.9 * y * (1 - y)
+        print("     ", x, "                    ", y)
 
 main()
---
+++ Python_Programming_John_Zelle/Chapter1/chaos1.py
@@ -0,0 +1,13 @@
+#File: chaos.py
+#A simple program illustratin chaotic behaviour
+
+def main():
+    print("This program illustrates a chaotic function")
+    n = eval(input("How many numbers should I print?"))
+    x = eval(input("Enter a number between 0 and 1: "))
+
+    for i in range(n):
+        x = 3.9 * (x - x * x)
+        print(x)
+
+main()
