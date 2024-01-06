c929c2d3b573eed541d671fbafafcf777d07b119
BillClintonAglomasa <aglomasa@yahoo.com>
2024-01-06 00:46:44 +0000

    Add match_statements

--- alx/for_statement1.py
+++ alx/for_statement1.py
@@ -6,3 +6,14 @@ words = ['cat', 'window', 'defenestrate']
 
 for w in words:
     print(w, len(w))
+
+users = {'Hans': 'active', 'Sasm': 'inactive', 'Jone': 'active'}
+
+for user, status in users.copy.item():
+    if user == 'active':
+        del users[user]
+
+active_users = {}
+for use, status in users.item():
+    if status == 'active':
+        active_users[user] = status
