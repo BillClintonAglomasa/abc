08e5201e5b426cbd0530c6f6101b3c1a699a6f58
BillClintonAglomasa <aglomasa@yahoo.com>
2024-01-06 17:08:55 +0000

    Add random_value_generator to generate int number within a range

---
+++ alx/learn_python_tutorial/random_value_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#import random module
+import random
+
+#generating random number
+num = random.randrange(1, 51)
+
+i = 0
+
+while (i != num):
+    i += 1
+
+print(f'The random number is {num}')
