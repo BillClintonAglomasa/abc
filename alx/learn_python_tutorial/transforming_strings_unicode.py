4e18b34da366778076af500ae64890415dca2fa0
BillClintonAglomasa <aglomasa@yahoo.com>
2024-01-07 01:16:52 +0000

    Add file to convert missed lower or upper word to unicode

--- alx/learn_python_tutorial/transforming_strings_unicode.py
+++ alx/learn_python_tutorial/transforming_strings_unicode.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
-#taking in input
 str_entered = input('Please enter string in uppercase: ')
 
 #creating an empty string and saving the unicode values
 str_unicode = ''
 for i in str_entered:
     #convert to unicode
-    str_unicode += str(ord(i))
+    str_unicode += str(ord(i) - 23)
 print('Encoded message is ', str_unicode)
 
 #converting the unicode back to characters
 new_str = ''
 i = 0
 while (i < len(str_unicode)):
-    new_str += chr(int(str_unicode[i:i+2]))
+    char = (str_unicode[i:i+2])
+    new_str += chr(int(char) + 23)
     i += 2
 
 #different implementation for assigning to new_str using for loop
@@ -22,7 +22,7 @@ other_str = ''
 len_str = len(str_unicode) - 1
 for i in range(0, len_str, 2):
     char = str_unicode[i] + str_unicode[i + 1]
-    other_str += chr(int(char))
+    other_str += chr(int(char) + 23)
 
 print('Original message', new_str)
 print('Other message ', other_str)
